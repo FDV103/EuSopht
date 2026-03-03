a3ec1d0958f1e14b3d5546313691f9a896f2174b
James Barbour <jameskbarbour1@gmail.com>
2026-03-03 18:48:31 -0500

    Disable pointer events

--- src/components/GridPattern.vue
+++ src/components/GridPattern.vue
@@ -7,8 +7,11 @@
 .grid-pattern {
   position: absolute;
   inset: 0;
-  background-image: linear-gradient(to right, var(--grid-colour) 1px, transparent 1px),
-                    linear-gradient(to bottom, var(--grid-colour) 1px, transparent 1px);
+  /* Need this so that hover etc events on other elements aren't blocked by this component */
+  pointer-events: none;
+  background-image:
+    linear-gradient(to right, var(--grid-colour) 1px, transparent 1px),
+    linear-gradient(to bottom, var(--grid-colour) 1px, transparent 1px);
   background-size: 4rem 4rem;
   mask-image: radial-gradient(ellipse 60% 50% at 50% 0%, #000 70%, transparent 100%);
 }
